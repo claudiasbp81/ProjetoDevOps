72e0a8e65698de2f7a057b14c025d3297d950682
claudiasbp81 <claudiasbp81@gmail.com>
2020-04-19 11:24:22 -0300

    Script para a execução da aplicação NODE

---
+++ playbooks/app_node/app.js
@@ -0,0 +1,13 @@
+
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000;
+app.listen(port);
+
+app.get('/api/v1/healthcheck', function(req, res) {
+  res.send({
+      "status": {
+          "app": true
+      }
+  });
+});
